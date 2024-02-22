ebe39d3a2eb3df4733649bfd4e117508ce4f0815
yendzevych_v <valya.zavoloka@gmail.com>
2024-02-22 21:08:55 +0200

    added new design, video

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,15 @@ $(function () {
 
   $('.hero__slider').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    fade: true,
+    autoplay: true
   });
 
+
+  Fancybox.bind('[data-fancybox]', {
+
+  });
+
+  var mixer = mixitup('.design__list');
 });
