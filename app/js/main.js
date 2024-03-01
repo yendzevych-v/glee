8310a826b90066acc4c470919154cd9bd2da4950
yendzevych_v <valya.zavoloka@gmail.com>
2024-03-01 21:56:25 +0200

    added page shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 $(function () {
 
+  $('.select-style').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $('.filter-price__from').text('$' + data.from.toFixed(2));
+      $('.filter-price__to').text('$' + data.to.toFixed(2));
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text('$' + data.from.toFixed(2));
+      $('.filter-price__to').text('$' + data.to.toFixed(2))
+    },
+  });
+
+  $(".star").rateYo({
+    readOnly: true,
+    starWidth: '10px',
+    spacing: '5px',
+    normalFill: '#d6d6d6',
+    ratedFill: '#ffcc00'
+  });
+
   $('.hero__slider').slick({
     dots: true,
     arrows: false,
@@ -7,19 +29,19 @@ $(function () {
     autoplay: true
   });
 
-
   Fancybox.bind('[data-fancybox]', {
 
   });
 
   var mixer = mixitup('section.product', {
     controls: {
-        scope: 'local'
+      scope: 'local'
     }
-});
+  });
+
   var mixer1 = mixitup('section.design', {
     controls: {
-        scope: 'local'
+      scope: 'local'
     }
-});
+  });
 });
